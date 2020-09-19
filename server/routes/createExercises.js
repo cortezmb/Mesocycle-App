4eb354bf9b9c4db139026e63424c77f3c36c34d9
cortezmb <michael.cortez78@gmail.com>
2020-09-19 13:43:57 -0500

    adding update route

--- server/routes/createExercises.js
+++ server/routes/createExercises.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models'); //Require db from models directory
-
+const bodyParser = require('body-parser');//parse the bodies of all incoming requests
+const auth = require('../auth/auth'); //function to protect the route
 
 //This CRUD operation will create attributes in database table from dropdown menu and input form
 router.post('/createExercises', async (req, res) => {
@@ -9,50 +10,101 @@ router.post('/createExercises', async (req, res) => {
     try {
             
             
-            let exercise = req.body.exercise;
-            let weight = req.body.weight;
-            let user_id = req.body.user_id;
-            let exercise_id = req.body.exercise_id;
-            let category_id = req.body.category_id
-            // // let results = await db.user.findAll({ where: {email: req.session.email}});
+        let exercise = req.body.exercise;
+        let weight = req.body.weight;
+        // let user_id = req.body.user_id;
+        let exercise_id = req.body.exercise_id;
+        let category_id = req.body.category_id
+        let results = await db.users.findAll({ where: {email: req.session.email}});
 
-            console.log(` exercise: ${exercise} weight: ${weight} exercise_id: ${exercise_id} category_id: ${category_id}`);
+        console.log(` exercise: ${exercise} weight: ${weight} exercise_id: ${exercise_id} category_id: ${category_id}`);
+
+        if (results.length > 0) {
 
             let data = await db.data.create({
 
-                    exercise: exercise,
-                    weight: weight,
-                    user_id: user_id,
-                    exercise_id: exercise_id,
-                    category_id: category_id
-            })
+                exercise: exercise,
+                weight: weight,
+                user_id: results[0].id,//first index of findall where email matches
+                exercise_id: exercise_id,
+                category_id: category_id
+        })
 
-            res.json(data);
-            
+        res.json(data);
+        }
+           
     } catch (error) {
             console.log('error inside of try catch', error)
     }
 })
 
 //This CRUD operation will read the data that was created in the data base
-router.get('/createExercises', async (req, res) => {
-try {
+router.get('/createExercises/:id', async (req, res) => {
 
-    
-    let allData = await db.data.findAll({
+    try {
 
-        where: {
-            user_id: 1,
-            exercise_id: 3,
-            category_id: 14
-        }
-    })
-    res.json(allData)
+        let user_id = req.params.id
 
+        console.log(user)
+
+        let inclinePushResults = await db.data.findAll({
+                
+            where: {
+                user_id: user_id,
+                exercise_id: 106,
+                category_id: 14
+            }
+        })
+
+        let chestIsolationResults =  await db.data.findAll({
+               
+            where: {
+                user_id: user,
+                exercise_id: 43,
+                category_id: 5
+            }
+        })
+
+        let horizontalPushResults =  await db.data.findAll(
+                {
+                    where: {
+                        user_id: user,
+                        exercise_id: 79,
+                        category_id: 11
+                    }
+                })
+
+        let rearOrSideDeltsResults =  await db.data.findAll(
+                {
+                    where: {
+                        user_id: user,
+                        exercise_id: 129,
+                        category_id: 17
+                    }
+                })
+
+        let horizontalPullResults =  await db.data.findAll(
+                {
+                    where: {
+                        user_id: user,
+                        exercise_id: 78,
+                        category_id: 10
+                    }
+                })
+        
+                res.json(
+                        {
+                            inclinePushResults: inclinePushResults,
+                            chestIsolationResults: chestIsolationResults,
+                            horizontalPushResults: horizontalPushResults,
+                            rearOrSideDeltsResults: rearOrSideDeltsResults,
+                            horizontalPullResults: horizontalPullResults
+                        })
+        
 } catch (error) {
-    console.log('error inside of try catch', error)
+        console.log('error inside of create catch', error);
 }
-    
+   
 })
 
 module.exports = router;
---
+++ server/routes/updateExercises.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models'); //Require db from models directory
+const bodyParser = require('body-parser');//parse the bodies of all incoming requests
+
+router.put('./updateExercises/:id', async (req, res) => {
+
+try {
+    let user_id = req.params.id
+    let exercise = req.body.exercise;
+    let weight = req.body.weight;
+    let exercise_id = req.body.exercise_id;
+    let category_id = req.body.category_id;
+    let results = await db.users.findAll({ where: {email: req.session.email}});
+
+    if (results.length > 0) {
+
+        let data = await db.data.update(
+            {
+                exercise: exercise,
+                weight: weight,
+                exercise_id: exercise_id,
+                category_id: category_id
+            },
+            {
+                where: {
+                    user_id: results[0].id
+                }
+            }
+        )
+    }
+
+} catch (error) {
+    console.log("this is an error message", error)
+}
+})
